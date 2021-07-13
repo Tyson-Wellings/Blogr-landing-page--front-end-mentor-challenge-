c29b46080f802e615474c70b963fe4c1601cef37
Tyson Wellings <tyson.s3510@gmail.com>
2021-07-13 19:02:18 +1000

    Finished mobile layout

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
 let nav = document.getElementById('nav');
-let x = document.getElementById('x')
-let hamburger = document.getElementById('hamburger')
+let x = document.getElementById('x');
+let hamburger = document.getElementById('hamburger');
+let dropdownItems = document.getElementsByClassName('category-items');
+let lightArrows = document.getElementsByClassName('light');
+let darkArrows = document.getElementsByClassName('dark')
 
 hamburger.addEventListener ("click", function(){
     hamburgerToggle(0)
@@ -25,4 +28,30 @@ function hamburgerToggle(state){
     }
 
     
+}
+
+let dropdownState = 0;
+
+function displayDropdown(index){
+    console.log('drop down state was '+ dropdownState)
+    if (dropdownState === 0){
+
+        dropdownItems[index].style.display ="block"
+        lightArrows[index].style.transform ="rotate(180deg)"
+        darkArrows[index].style.transform ="rotate(180deg)"
+        dropdownState = 1
+        console.log('drop down state is '+ dropdownState)
+        return
+    }
+
+    if(dropdownState === 1){
+        for (let i=0; i<3; i++){
+            dropdownItems[i].style.display ="none"
+            lightArrows[i].style.transform ="rotate(0deg)"
+            darkArrows[i].style.transform ="rotate(0deg)"
+        }
+        dropdownState = 0
+        console.log('drop down state is '+ dropdownState)
+        return
+    }
 }
